7b629a2b2ade31dcdaa9c5559a20487e6082c243
Dragan Kukolj <kukolj.dragan@gmail.com>
2023-03-01 19:24:14 +0100

    Commit

--- my-app/src/pages/SignInPage.jsx
+++ my-app/src/pages/SignInPage.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { InputFormField } from "../components/InputFormField";
 import { SubmitFormField } from "../components/SubmitFormField";
 
 export function SignInPage() {
+    const [ formState, setFormState ] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState);
+    }
+
+    function handleUsernameChange(value) {
+        setFormState(value);
+    }
+
+    function handlePasswordChange(value) {
+        setFormState(value);
+    }
+
     return (
         <div className="sign-in-page">
             <div className="card">
-                <form className="sign-in-form">
-                    <InputFormField label="Username" type="text" />
-                    <InputFormField label="Password" type="password" />
+                <form className="sign-in-form" onSubmit={handleSubmit}> 
+                    <InputFormField label="Username" type="text" onChange={handleUsernameChange}/>
+                    <InputFormField label="Password" type="password" onChange={handlePasswordChange} />
                     <SubmitFormField />
                 </form> 
             </div>
