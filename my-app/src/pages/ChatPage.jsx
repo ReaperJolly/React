6488d79c1ffe5e0a733f3d99687941586b0dddfc
Dragan Kukolj <kukolj.dragan@gmail.com>
2023-03-13 19:34:45 +0100

    Commit

--- my-app/src/pages/ChatPage.jsx
+++ my-app/src/pages/ChatPage.jsx
@@ -3,7 +3,7 @@ import { SubmitFormField } from "../components/SubmitFormField";
 import { Message } from "../components/Message";
 import { useState } from "react";
 
-const messages = [
+/* const messages = [
     {
         id: "1234", 
         author: {
@@ -20,18 +20,30 @@ const messages = [
         },
         text: "Hey Jack",
     },
-];
+]; */
 
 
-export function ChatPage() {
+export function ChatPage(props) {
 
     const [ formState, setFormState ] = useState(``);
+    const [ messages, setMessages ] = useState([]);
 
     function handleChange(message) {
         setFormState(message);
     }
 
-    console.log(formState);
+    function handleSubmit(event) {
+        event.preventDefault();
+        setFormState(``);
+        setMessages([...messages, {
+            id: Date.now(), 
+        author: {
+            username: props.username,
+            avatarIndex: props.avatarIndex,
+        },
+        text: formState,
+        }]);
+    }
    
     const messageComponents = messages.map((message) => {
         return <Message key={message.id} avatarIndex={message.author.avatarIndex} author={message.author.username} text={message.text} />
@@ -43,7 +55,7 @@ export function ChatPage() {
             <div className="message-list"> 
                 {messageComponents}
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <InputFormField label="Message" type="text" value={formState} onChange={handleChange} />
                 <SubmitFormField label="Send"/>
             </form>
--- my-app/src/pages/SignInPage.jsx
+++ my-app/src/pages/SignInPage.jsx
@@ -12,7 +12,10 @@ export function SignInPage(props) {
 
     function handleSubmit(event) {
         event.preventDefault();
-        props.onSubmit(formState);
+        props.onSubmit({
+            username: formState,
+            avatarIndex: avatar,
+        });
     }
 
     function handleUsernameChange(value) {
